93eded0ceb9c569861cb36ae99f7334a667ecf74
wichayaparamee <151129264+wichayaparamee@users.noreply.github.com>
2023-11-17 15:47:03 +0700

    Add files via upload

--- GUI_dailyprograme.py
+++ GUI_dailyprograme.py
@@ -16,22 +16,29 @@ def Button1():
 
 FB1=LabelFrame(GUI,text='normal')
 FB1.place(x=10,y=50)
-B1 = ttk.Button(FB1,text='วันจันทร์') # หากใช้ ttk จะไม่สามารถกำหนดลักษณะ font ได้ 
+B1 = ttk.Button(FB1,text='วันจันทร์',command=Button1) # หากใช้ ttk จะไม่สามารถกำหนดลักษณะ font ได้ 
 
 B1.pack(ipadx=40,ipady=10)
 
+def Button2():
+    text='run 10 km'
+    messagebox.showinfo('programe',text)
 
 FB2=LabelFrame(GUI,text='normal')
 FB2.place(x=10,y=150)
-B2 = Button(FB2,text='วันอังคาร',font=('Angsana New',20),fg='pink')
+B2 = Button(FB2,text='วันอังคาร',font=('Angsana New',20),fg='pink',command=Button2)
 B2.pack(ipadx=40,ipady=10)
 
+def Button3():
+    text='run 5 km'
+    messagebox.showinfo('programe',text)
+    
 FB3=LabelFrame(GUI,text='normal')
 FB3.place(x=10,y=250)
-B3 = Button(FB3,text='วันพุธ',font=('Angsana New',20),fg='green')
+B3 = Button(FB3,text='วันพุธ',font=('Angsana New',20),fg='green',command=Button3)
 B3.pack(ipadx=40,ipady=10)
 
 FB4=LabelFrame(GUI,text='rest')
 FB4.place(x=10,y=350)
-B4 = Button(FB4,text='วันพฤหัส',font=('Angsana New',20),fg='orange')
+B4 = Button(FB4,text='วันศุกร์นะ แก้ไข',font=('Angsana New',20),fg='orange')
 B4.pack(ipadx=40,ipady=10)
